d8edd5d9e1af2673572b498a4a6b71840311f4d5
happyquokkka <yjmooon96@gmail.com>
2021-08-08 16:36:46 +0900

    0808 CSS 연습문제 공부내용

---
+++ Front_end/projectEX/WebContent/js/slideShow.js
@@ -0,0 +1,59 @@
+/**
+ * slideShow.js
+ */
+
+$(function() {
+    // 초기 슬라이더 위치 랜덤하게 지정
+    var randomNumber = Math.floor(Math.random() * 5);
+    moveSlide(randomNumber);
+
+    // 이동한 이미지의 index 값 저장 (현재 위치)
+    var movedIndex = 0;
+
+    // 슬라이드 패널을 움직여주는 함수
+    function moveSlide(index) {
+        // 전달 받은 index 값을 moveIndex에 저장
+        movedIndex=index;
+
+        // 슬라이드 이동
+        var moveLeft = -(index*1280); // 왼쪽으로 이동 거리
+        $('#slidePanel').animate({'left':moveLeft},'slow');
+    }
+
+    // prev 버튼 클릭하면 앞으로 이동
+    $('#prevButton').on('click',function() {
+        if(movedIndex!=0) // 첫 번째가 아니라면
+            movedIndex = movedIndex -1; // 인덱스 값 -1
+
+        moveSlide(movedIndex);
+    });
+
+    // next 버튼 클릭하면 뒤로 이동
+    $('#nextButton').on('click',function() {
+        if(movedIndex!=4) // 마지막이 아니라면
+            movedIndex = movedIndex+1; // 인덱스 값 + 1
+
+        moveSlide(movedIndex); // 인덱스 값 전달
+    });
+
+    $('.controlButton').each(function(index) {
+       $(this).hover(
+           function(){
+              $(this).attr('src','../image/controlButton2.png');
+           },
+           function(){
+               $(this).attr('src','../image/controlButton1.png');
+           }); // hover끝
+
+        // 클릭하면 현재 인덱스 값을 moveslide() 함수에 전달
+        $(this).on('click',function() {
+            moveSlide(index);
+        });
+
+
+       }); // each 함수 끝
+
+
+
+}); // 종료
+
